393c9c2f8193228c7cefe76ae99cf9838e8ecda7
Jacob Burenstam <burenstam@gmail.com>
2014-09-12 00:18:06 +0200

    Added `icon` helper.
    Adds a level of abstraction to the existing `glyphicon` helper, allowing
    users to select a different vector icons library. For instance, it is
    now possible to display Font Awesome icons by passing the library option:
    
        <%= icon :heart, library: :font_awesome %>
    
    Note that, since Font Awesome CSS is not loaded by default with Bootstrap
    CSS, it's up to the user to make sure the CSS is loaded in the same app.

--- lib/bh/helpers/glyphicon_helper.rb
+++ lib/bh/helpers/glyphicon_helper.rb
@@ -9,16 +9,16 @@ module Bh
     # Returns an HTML block tag that follows the Bootstrap documentation
     # on how to display *glyphicons*.
     # @return [String] an HTML block tag for a glyphicon.
-    # @param [#to_s] the name of the icon to display, with either dashes or
-    #   underscores to separate multiple words.
+    # @param [#to_s] :name the name of the icon to display, with either dashes
+    #   or underscores to separate multiple words.
+    # @param [Hash] options the options passed to the HTML tag that displays
+    #   the icon.
     # @example Display the "zoom in" glyphicon
     #   glyphicon :zoom_in
     # @example Display the "zoom out" glyphicon
     #   glyphicon 'zoom-out'
     def glyphicon(name = nil, options = {})
-      append_class! options, 'glyphicon'
-      append_class! options, "glyphicon-#{name.to_s.gsub '_', '-'}" if name
-      content_tag :span, nil, options
+      icon name, options.merge(library: :glyphicons)
     end
   end
 end
