9494617fe2c891105ef9eae2e0bb4efb95b170e5
Jacob Burenstam <burenstam@gmail.com>
2014-09-12 01:05:31 +0200

    Added support for Font Awesome option classes

--- lib/bh/helpers/font_awesome_helper.rb
+++ lib/bh/helpers/font_awesome_helper.rb
@@ -13,10 +13,23 @@ module Bh
     #   font_awesome :zoom_in
     # @example Display the "zoom out" icon
     #   font_awesome 'zoom-out'
+    # @example Display the "zoom out" icon with 2 times the size
+    #   font_awesome 'zoom-out', size: 2
+    # @example Display the "zoom out" icon with fixed width
+    #   font_awesome 'zoom-out', fixed: true
     def font_awesome(name = nil, options = {})
       append_class! options, 'fa'
       append_class! options, "fa-#{name.to_s.gsub '_', '-'}" if name
-      content_tag :i, nil, options
+      append_class_and_delete_option!(options, :size, "fa-#{options[:size]}x") if options[:size]
+      append_class_and_delete_option!(options, :fixed, 'fa-fw') if options[:fixed]
+      content_tag :span, nil, options
+    end
+
+  private
+
+    def append_class_and_delete_option!(options, param, value)
+      append_class! options, value
+      options.delete(param)
     end
   end
 end
--- spec/helpers/font_awesome_helper_spec.rb
+++ spec/helpers/font_awesome_helper_spec.rb
@@ -4,18 +4,26 @@ include Bh::FontAwesomeHelper
 
 describe 'font_awesome' do
   specify 'given an icon name with underscores, returns the icon' do
-    expect(font_awesome :fire).to eq '<i class="fa fa-fire"></i>'
+    expect(font_awesome :fire).to eq '<span class="fa fa-fire"></span>'
   end
 
   specify 'given an icon name with dashes, returns the icon' do
-    expect(font_awesome 'fire_extinguisher').to eq '<i class="fa fa-fire-extinguisher"></i>'
+    expect(font_awesome 'fire_extinguisher').to eq '<span class="fa fa-fire-extinguisher"></span>'
   end
 
   specify 'given no icon name, returns an empty icon' do
-    expect(font_awesome).to eq '<i class="fa"></i>'
+    expect(font_awesome).to eq '<span class="fa"></span>'
   end
 
-  specify 'given extra options, passes the options to the <i>' do
-    expect(font_awesome :ok, title: 'Approved').to eq '<i class="fa fa-ok" title="Approved"></i>'
+  specify 'given extra options, passes the options to the <span>' do
+    expect(font_awesome :ok, title: 'Approved').to eq '<span class="fa fa-ok" title="Approved"></span>'
+  end
+
+  specify 'given size option, appends size class to the <span>' do
+    expect(font_awesome :ok, size: 2).to eq '<span class="fa fa-ok fa-2x"></span>'
+  end
+
+  specify 'given fixed option, appends fixed width class to the <span>' do
+    expect(font_awesome :ok, fixed: true).to eq '<span class="fa fa-ok fa-fw"></span>'
   end
 end
