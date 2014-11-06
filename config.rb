26b1337900821f36e9bb511ef2632bf5960f02f3
claudiob <claudiob@gmail.com>
2014-11-06 09:44:53 -0800

    Move example app from spec/dummy to examples/

--- config.rb
+++
@@ -1,6 +0,0 @@
-# If you are developing new helpers and want a quick way to see them in
-# a browser, type `bundle exec middleman` and browse to http://localhost:4567
-# The browser will show the interpolated content of spec/dummy/index.html.erb
-# which you are free to edit as you please to display new helpers.
-set :source, 'spec/dummy'
-activate :bh
