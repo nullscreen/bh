7a4b40295494b505672ebc4d2a7d298f66b3f4f3
claudiob <claudiob@gmail.com>
2014-11-06 14:37:01 -0800

    Reduce public API methods from ButtonHelper
    Before this PR, including `bh` in an app would include more methods
    than necessary for buttons: methods like `button_string` or
    `btn_class`, that should only be accessed privately.
    
    This PR extracts those private methods into a new Button class
    that includes all the business logic to render elements and edit
    attributes (e.g., attach classes), leaving the helper cleaner.

--- lib/bh/helpers/button_helper.rb
+++ lib/bh/helpers/button_helper.rb
@@ -1,63 +1,39 @@
-require 'bh/helpers/base_helper'
+require 'bh/classes/button'
 
 module Bh
-  # Provides methods to include buttons.
-  # @see http://getbootstrap.com/components/#buttons
+  # Provides the `button` helper.
   module ButtonHelper
-    include BaseHelper
-
-    # Returns an HTML block tag that follows the Bootstrap documentation
-    # on how to display *buttons*.
-    #
-    # The content of the button can either be passed as the first parameter (in
-    # which case, the options are the second parameter), or as a block (in
-    # which case, the options are the first paramter).
-    # @example An button with plain-text content passed as the first parameter.
-    #   <%= button 'Your profile was updated!', context: :info %>
-    # @example A button with HTML content passed as a block.
-    #   <%= button context: :info %>
-    #     <%= glyphicon :star %> Star
-    #   <% end %>
-    #
-    # @return [String] an HTML block tag for a button.
-    # @param [String] content_or_options_with_block the content to display in
-    #   the button.
-    # @param [Hash] options the display options for the button.
-    # @option options [#to_s] :context (:default) the contextual alternative to
-    #   apply to the button depending on its importance. Can be :default,
-    #   :primary, :success, :info, :warning, :danger or :link.
-    # @option options [#to_s] :size the size of the button.
-    # @option options [#to_s] :layout if set to :block, span the button for
+    # @see http://getbootstrap.com/css/#buttons
+    # @return [String] an HTML block to display a button.
+    # @overload button(caption, options = {})
+    #   @param [#to_s] caption the caption to display in the button.
+    #   @param [Hash] options the options for the button.
+    #   @option options [#to_s] :context (:default) the contextual alternative
+    #     to apply to the button. Can be `:danger`, `:info`, `:link`,
+    #     `:primary`, `:success` or `:warning`.
+    #   @option options [#to_s] :size the size of the button.
+    #     Can be `:extra_small` (alias `:xs`), `:large` (alias `:lg`) or
+    #     `:small` (alias `:sm`).
+    #   @option options [#to_s] :layout if set to `:block`, span the button for
     #   the full width of the parent.
-    def button(content_or_options_with_block = nil, options = nil, &block)
-      if block_given?
-        button_string capture(&block), content_or_options_with_block || {}
-      else
-        button_string content_or_options_with_block, options || {}
-      end
-    end
+    #   @example Display a small warning button with a plain-text caption.
+    #       button 'Delete', context: :warning, size: :small
+    # @overload button(options = {}, &content_block)
+    #   @param [Hash] options the options for the button.
+    #   @yieldreturn [#to_s] the caption to display in the button.
+    #   @example Display a button with an HTML message.
+    #       button do
+    #         content_tag :strong, "Click here"
+    #       end
+    def button(*args, &block)
+      button = Bh::Button.new(self, *args, &block)
+      button.extract! :context, :size, :layout
 
-  private
-
-    def button_string(content = nil, options = {})
-      append_class! options, btn_class(options)
-      content_tag :button, content, options
-    end
-
-    def btn_class(options = {})
-      valid_contexts = %w(primary success info warning danger link)
-      context = context_for options.delete(:context), valid: valid_contexts
-      "btn btn-#{context}"
-
-      size = case options.delete(:size).to_s
-        when 'lg', 'large' then 'btn-lg'
-        when 'sm', 'small' then 'btn-sm'
-        when 'xs', 'extra_small' then 'btn-xs'
-      end
-
-      layout = 'btn-block' if options.delete(:layout).to_s == 'block'
-
-      ['btn', "btn-#{context}", size, layout].compact.join ' '
+      button.append_class! :btn
+      button.append_class! button.context_class
+      button.append_class! button.size_class
+      button.append_class! button.layout_class
+      button.render_tag :button
     end
   end
 end
