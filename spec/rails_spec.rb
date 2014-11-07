5ed9b16758b933872c9975a01c49eb6dccb02192
claudiob <claudiob@gmail.com>
2014-11-07 10:34:22 -0800

    Reduce duplication when testing CDN helpers

--- spec/rails_spec.rb
+++ spec/rails_spec.rb
@@ -5,8 +5,12 @@ describe 'When used in Rails' do
   include ActionView::Context # for capture
 
   all_tests_pass_for 'the alert_box helper'
+  all_tests_pass_for 'the bootstrap_css helper'
+  all_tests_pass_for 'the bootstrap_js helper'
+  all_tests_pass_for 'the bootstrap_theme_css helper'
   all_tests_pass_for 'the button helper'
   all_tests_pass_for 'the dropdown helper'
+  all_tests_pass_for 'the font_awesome_css helper'
   all_tests_pass_for 'the panel helper'
   all_tests_pass_for 'the panel_row helper'
 end
