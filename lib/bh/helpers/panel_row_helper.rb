cdc13206bb28cb1a8614e2350303e25cf375cc92
claudiob <claudiob@gmail.com>
2014-11-12 15:08:25 -0800

    Add panel_row documentation

--- lib/bh/helpers/panel_row_helper.rb
+++ lib/bh/helpers/panel_row_helper.rb
@@ -2,19 +2,22 @@ require 'bh/classes/panel_row'
 
 module Bh
   module Helpers
+    # Wraps a set of Bootstrap-styled panels in a row.
     # @see http://getbootstrap.com/components/#panels
     # @see http://getbootstrap.com/css/#grid
-    # @return [String] an HTML block to display a row of panels.
-    # @example An row of 3 panels with the same width.
-    #   panel_row column_class: 'col-sm-4' do
-    #     panel body: 'Panel #1'
-    #     panel body: 'Panel #2'
-    #     panel body: 'Panel #3'
-    # @param [Hash] options the display options for the row of panels.
-    # @option options [#to_s] :column_class the class to wrap each panel with.
-    #   Useful to specify a grid size for the column such as 'col-sm-4' to
-    #   indicate how many columns of the row each panel should occupy.
-    # @yieldreturn [#to_s] the panels to display in a row.
+    # @return [String] the HTML to display a row of Bootstrap-styled panels.
+    # @overload panel_row(options = {}, &block)
+    #   @param [Hash] options the options for the row. Any option not
+    #     listed below is passed as an HTML attribute to the row’s `<div>`.
+    #   @option options [#to_s] :column_class the class to wrap each panel with.
+    #     Useful to specify a grid size for the column such as 'col-sm-4' to
+    #     indicate how many columns of the row each panel should occupy.
+    #   @yieldreturn [#to_s] the panels to display in a row.
+    #   @example Display a row of two panels with the same width.
+    #     panel_row column_class: 'col-sm-6' do
+    #       panel 'Panel #1', context: :success
+    #       panel 'Panel #2', context: :info
+    #     end
     def panel_row(*args, &block)
       panel_row = Bh::PanelRow.new self, *args, &block
       panel_row.extract! :column_class
