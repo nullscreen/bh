dd42bd0bfe24deed12118e74b7f3851874894c03
claudiob <claudiob@gmail.com>
2014-11-12 16:10:45 -0800

    Add horizontal/vertical documentation

--- lib/bh/helpers/horizontal_helper.rb
+++ lib/bh/helpers/horizontal_helper.rb
@@ -2,6 +2,27 @@ require 'bh/classes/navbar'
 
 module Bh
   module Helpers
+    # Displays the collapsable portion of a Bootstrap-styled navbar.
+    # @see http://getbootstrap.com/components/#navbar
+    # @return [String] the HTML to display the collapsable portion of a
+    #   Bootstrap-styled navbar.
+    # @overload horizontal(content, options = {})
+    #   @param [#to_s] content the collapsable content to display in the navbar.
+    #   @param [Hash] options the options to pass to the wrapping `<div>`.
+    #     Note that the `:id` option is ignored since the id must generated
+    #     by the navbar in order to match with the target of the toggle button.
+    # @overload horizontal(options = {}, &block)
+    #   @param [Hash] options the options to pass to the wrapping `<div>`.
+    #   @yieldreturn [#to_s] the collapsable content to display in the navbar.
+    # @example Display a navbar with two collapsable links.
+    #   navbar do
+    #     horizontal do
+    #       nav do
+    #         link_to 'Home', '/'
+    #         link_to 'Profile', '/profile'
+    #       end
+    #     end
+    #   end
     def horizontal(*args, &block)
       if navbar = Bh::Stack.find(Bh::Navbar)
         horizontal = Bh::Base.new self, *args, &block
--- lib/bh/helpers/vertical_helper.rb
+++ lib/bh/helpers/vertical_helper.rb
@@ -3,6 +3,24 @@ require 'bh/classes/vertical'
 
 module Bh
   module Helpers
+    # Displays the non-collapsable portion of a Bootstrap-styled navbar.
+    # @see http://getbootstrap.com/components/#navbar
+    # @return [String] the HTML to display the non-collapsable portion of a
+    #   Bootstrap-styled navbar.
+    # @overload vertical(content, options = {})
+    #   @param [#to_s] content the non-collapsable content to display in the
+    #     navbar.
+    #   @param [Hash] options the options to pass to the wrapping `<div>`.
+    # @overload vertical(options = {}, &block)
+    #   @param [Hash] options the options to pass to the wrapping `<div>`.
+    #   @yieldreturn [#to_s] the non-collapsable content to display in the
+    #     navbar.
+    # @example Display a navbar a non-collapsable links.
+    #   navbar do
+    #     vertical do
+    #       link_to 'Home', '/'
+    #     end
+    #   end
     def vertical(*args, &block)
       if navbar = Bh::Stack.find(Bh::Navbar)
         vertical = Bh::Vertical.new self, *args, &block
